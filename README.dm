d1193d1f875e98e4a662fdf2cd421341603d8003
nguyen xuan hung <nguyenxuanhung310@gmail.com>
2020-04-13 11:04:55 +0700

    migation database

--- README.dm
+++ README.dm
@@ -1,5 +1,10 @@
 # ASP.NET 
 ## Technologies
--	ASP
+-	ASP .NET Core 3.1
+-	Entity Framework core 3.1
+## Install Packages
+-	Microsoft.EntityFrameworkCore.SqlServer
+	Microsoft.EntityFrameworkCore.Design
+	Microsoft.EntityFrameworkCore.Tools
 ## How to configure and run
 ## How to Contribute
---
+++ eShopSolution.Data/Configurartions/TransactionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using Transaction = eShopSolution.Data.Entities.Transaction;
+
+namespace eShopSolution.Data.Configurartions
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+        }
+
+    }
+}
